5810d9133092bfabb60404f6c9d103486f38f2ae
Siddharth Shekhar <30648642+Sid0518@users.noreply.github.com>
2020-07-26 20:18:43 +0530

    Added Firestore attributes to User class

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -8,11 +8,52 @@ import 'package:menu_scanner/imports.dart';
 class User extends ChangeNotifier {
   FirebaseUser _firebaseUser;
   User({FirebaseUser firebaseUser}) : this._firebaseUser = firebaseUser;
+  
+  Map<String, dynamic> userData;
 
-  set firebaseUser(FirebaseUser firebaseUser) {
+  Future<void> fetchData() async {
+    this.userData = 
+      (await Firestore.instance
+        .collection('users')
+        .document(this.id)
+        .get()).data;
+  }
+
+  void updateData({  
+    String ownerName, String restaurantName, 
+    String contactNo,
+    String addressLine1, String addressLine2, 
+    String city, String pinCode,
+    String state,
+  }) {
+    this.userData['ownerName'] = ownerName;
+    this.userData['restaurantName'] = restaurantName;
+    this.userData['contactNo'] = contactNo;
+
+    this.userData['addressLine1'] = addressLine1;
+    this.userData['addressLine2'] = addressLine2;
+
+    this.userData['city'] = city;
+    this.userData['pinCode'] = pinCode;
+
+    this.userData['state'] = state;
+
+    this.userData['slug'] = createSlug(
+      restaurantName: restaurantName,
+      addressLine1: addressLine1,
+      addressLine2: addressLine2,
+      city: city
+    );
+  }
+
+  Future<void> initialize(FirebaseUser firebaseUser) async {
     this._firebaseUser = firebaseUser;
+    await this.fetchData();
+
     notifyListeners();
   }
+
+  String getAttribute(String attribute) => this.userData[attribute];
 
   // Added a few nifty getters to make life easy
   FirebaseUser get firebaseUser => this._firebaseUser;
